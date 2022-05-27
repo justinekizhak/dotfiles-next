0c49b77c578d8a9a1490ef4358414154dd8506e1
Justine Kizhakkinedath <justine@kizhak.com>
2022-05-28 03:29:39 +0530

    Add package check before installation

---
+++ install_package.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+import argparse
+import subprocess
+import sys
+
+def checkIfCommandExists (command_name: str, install_script: str, is_brew: bool, custom_check: str):
+  command_check = f"command -v {command_name}"
+  if custom_check != None:
+    command_check = custom_check
+  cmd = subprocess.Popen(command_check, stdout=subprocess.PIPE, shell=True)
+  cmd_out, cmd_err = cmd.communicate()
+  cmd_out = cmd_out.decode(sys.stdout.encoding).strip()
+  is_installed = cmd_out != ""
+  if is_installed:
+    print(f"--- {command_name} is already installed")
+  else:
+    if is_brew:
+      install_script = f"brew install {install_script or command_name}"
+    subprocess.run(install_script, shell=True)
+
+
+parser = argparse.ArgumentParser(description='Check and install packages')
+parser.add_argument('command_name',  type=str, help='command name')
+parser.add_argument('install_script',  type=str, help='install script', nargs='?')
+parser.add_argument("-b", "--brew", action='store_true')
+parser.add_argument('custom_check',  type=str, help='custom_check', nargs='?')
+
+args = parser.parse_args()
+
+checkIfCommandExists(args.command_name, args.install_script, args.brew, args.custom_check)
